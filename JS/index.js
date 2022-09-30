d542c6e7862300a39616ea33c964ed4a7e791dcf
Emilio Lubo <emilubo@msn.com>
2022-09-30 00:43:35 -0300

    practicas js

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+/* let miNombre = prompt('Ingrese su nombre')
+let miApellido = prompt('Ingrese su apellido')
+let miEdad = Number(prompt('Ingrese su edad'))
+let miMascota = prompt('Ingrese el nombre de su mascota')
+let edadMascota = Number(prompt('Ingrese la edad de su mascota'))
+console.log(miNombre)
+console.log(miApellido)
+console.log(miEdad)
+console.log(miMascota)
+console.log(edadMascota)
+let nombreCompleto = miNombre + ' ' + miApellido
+let textoPresentacion = 'Mi nombre es ' + nombreCompleto + '. Tengo ' + miEdad + ' anios y una mascota llamada ' + miMascota + ' de ' + edadMascota + ' anios'
+console.log(textoPresentacion)
+let sumaEdades = miEdad + edadMascota
+let restaEdades = miEdad - edadMascota
+let productoEdades = miEdad * edadMascota
+let divisionEdades = miEdad / edadMascota
+console.log(sumaEdades)
+console.log(restaEdades)
+console.log(productoEdades)
+console.log(divisionEdades) */
+/* 11.    Crear un objeto llamado alumno con un mínimo de 5 propiedades, mostrar dicho objeto utilizando console.table() y también mostrar cada una de las propiedades del objeto por separado.
+12.    Crear un objeto llamado mascota con un mínimo de 5 propiedades, mostrar dicho objeto utilizando console.table() y también mostrar cada una de las propiedades del objeto por separado.
+13.    Crear un array llamado frutas con un mínimo de 5 elementos y mostrar por consola el array completo y cada uno de los elementos por separado.
+14.    Crear un array llamado números con un mínimo de 5 elementos y mostrar por consola el array completo y cada uno de los elementos por separado.
+15.    Crear un array llamado familia con un mínimo de 5 objetos y mostrar por consola el array completo y cada uno de los elementos por separado.
+16.    Crear una variable llamada textoAleatorio formando una frase con el segundo elemento del array del punto 13, el cuarto elemento del punto 14 y el quinto objeto del array del punto 15. */
